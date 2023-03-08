a124e030d9cad9b222f2e80d2cd149644566f9b8
Entouanes <coma@NB29629.elcaNet.local>
2023-03-08 14:27:57 +0100

    TableOfActions updated

--- src/components/TableOfActions.tsx
+++ src/components/TableOfActions.tsx
@@ -1,7 +1,22 @@
 import React from "react"
 import { AttemptType } from "../types";
+import { TaskRow } from "./Timeline/MyVirtualizedTimeline";
 
-const TableOfActions = (_props: { attempt: AttemptType; }) => {
+function createData(rows : TaskRow[]) {
+    console.log(rows.map(taskRow => {
+        return {
+            actionName: taskRow.data[0].step_name,
+            
+        };
+    }))
+    return {
+        
+    }
+}
+
+
+const TableOfActions = (props: { attempt: AttemptType; }) => {
+    createData(props.attempt.rows)
     return ( 
         <b>Hey</b>
      );
